d65a39ac0795c69f8cb4153ead883317a01e4663
Hernan Quintana <hernan@jsatch.com>
2022-11-09 08:22:08 -0500

    Fixing problems with gamepad

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -54,7 +54,7 @@ public class PlayerController : MonoBehaviour
     public void ActivateHitbox()
     {
         hitBox.SetActive(true);
-    }
+    }   
 
     public void OnAttackAnimationEnd()
     {
